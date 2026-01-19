431e831b2ebdce53a3524db86b6a831a71ebdbee
krupa-1926 <patelkrupa1926@gmail.com>
2026-01-19 18:16:39 +0530

    Fix auth middleware export

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { getItemFromLocalStorage } from '@/utils'
-axios.defaults.withCredentials = true;
+// axios.defaults.withCredentials = true;
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:4000",
